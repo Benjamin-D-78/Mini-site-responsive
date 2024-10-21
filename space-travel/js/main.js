fe8ab423dd0d4819f8720691aef357df18e36b69
Benjamin DESMONET <desmonet.pro@laposte.net>
2024-10-21 18:50:28 +0200

    update and add folders

--- space-travel/js/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    const menuGlobal = document.querySelector('#global-menu');
-    const btnMenu = menuGlobal.children[0];
-
-    btnMenu.addEventListener('click',function(){
-        menuGlobal.classList.toggle('open');
-    });
-}
